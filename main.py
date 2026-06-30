df3266a8952d0a0946e598d09cb3efc1d45df69d
DipeshOli07 <odipesh821@gmail.com>
2026-06-30 08:11:12 +0545

    Basic Setup

---
+++ main.py
@@ -0,0 +1,29 @@
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+from inngest.experimental import ai
+import uuid
+import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id="rag_app",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+    fn_id="RAD: Ingest PDF",
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def rag_ingest_pdf(ctx:inngest.Context):
+    return {"hello" : "World"}
+
+app = FastAPI()
+
+inngest.fast_api.serve(app, inngest_client, functions=[rag_ingest_pdf])
